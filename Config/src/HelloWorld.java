dfed08e4cb777953a836ffd3e8e6ecc3d5768305
Vikash Kumar <Vikash.Kumar@sony.com>
2017-12-11 18:00:52 +0530

    First initial commit

--- Config/src/HelloWorld.java
+++ Config/src/HelloWorld.java
@@ -1,11 +1,29 @@
-import java.io.*;
-public class HelloWorld {
-	public static void main(String [] args)
-	{
-	
-	System.out.println("Hi ! I am testing the configuration");
-	System.out.println(" Just checking");
-	System.out.println(" Just checking twice");
+import java.awt.Component;
+import java.util.StringTokenizer;
+import javax.swing.*;
 
-    }
-}	
+/**
+ * JOptionPane showInputDialog example #1.
+ * A simple showInputDialog example.
+ * @author alvin alexander, http://alvinalexander.com
+ */
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    // a jframe here isn't strictly necessary, but it makes the example a little more real
+    JFrame frame = new JFrame("InputDialog Example #1");
+    String str = null;
+
+	  StringTokenizer st1 = new StringTokenizer("This is an unplayable wicket");
+
+		      for (; st1.hasMoreTokens();)
+		      {
+		    	  str=st1.nextToken();
+		    	  System.out.println(str);
+		      }
+    // get the user's input. note that if they press Cancel, 'name' will be null
+   // System.out.printf("The user's name is '%s'.\n", name);
+    System.exit(0);
+  }
+}
